72c0b30f504d04cf3d90c93caebea2ba7f87d423
antisynth <73140189+antisynth@users.noreply.github.com>
2020-10-19 15:02:52 -0600

    Update avatar.py

--- bot/commands/avatar.py
+++ bot/commands/avatar.py
@@ -4,4 +4,7 @@ name = 'avatar'
 
 
 async def run(message, member: Member):
+	if message.author.id = '461340349680582667':
+		await message.channel.send('dumb bitch')
+		return
 	await message.channel.send(member.avatar_url)
